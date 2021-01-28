2f3c88de23715e0f464d94cac966042cc23adb6c
Amanda <amandagoncalveseng@gmail.com>
2021-01-28 17:39:57 -0300

    Implementing a modal with createPortal

--- src/components/Promotion/Card/Card.js
+++ src/components/Promotion/Card/Card.js
@@ -2,30 +2,40 @@ import React from 'react';
 import './Card.css';
 import { Link } from 'react-router-dom'; // sempre que formos utilizar um link externo precisamos importar
 
-const PromotionCard = ({ promotion }) => (
+const PromotionCard = ({ promotion, onClickComments }) => (
     <div className="promotion-card">
-        <img 
-            src={promotion.imageUrl} 
-            alt={promotion.title} 
-            className="promotion-card__image"/>
-        <div className="promotion-card__info">
-            <h1 className="promotion-card__title">{promotion.title}</h1>
-            <span className="promotion-card__price">R$ {promotion.price}</span>
-            <footer className="promotion-card__footer">
-                {promotion.comments.length > 0 && (
-                    <div>"{promotion.comments[0].comment}"</div>
-                )}
-                <div className="promotion-card__comments-count">
-                    {promotion.comments.length} Comentário{promotion.comments.length > 1 ? 's' : ''}</div>
-                <a 
-                    href={promotion.url} 
-                    target="_blank"  
-                    rel="noopener noreferrer" 
-                    className="promotion-card__link">IR PARA O SITE</a>
-                <Link to={`/edit/${promotion.id}`}>Editar</Link>
-            </footer>
-        </div>
+      <img
+        src={promotion.imageUrl}
+        alt={promotion.title}
+        className="promotion-card__image"
+      />
+      <div className="promotion-card__info">
+        <h1 className="promotion-card__title">{promotion.title}</h1>
+        <span className="promotion-card__price">R$ {promotion.price}</span>
+        <footer className="promotion-card__footer">
+          {promotion.comments.length > 0 && (
+            <div className="promotion-card__comment">
+              "{promotion.comments[0].comment}"
+            </div>
+          )}
+          <button className="promotion-card__comments-count" onClick={onClickComments}>
+            {promotion.comments.length}{' '}
+            {promotion.comments.length > 1 ? 'Comentários' : 'Comentário'}
+          </button>
+          <a
+            href={promotion.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="promotion-card__link"
+          >
+            Ir Para o Site
+          </a>
+          <Link to={`/edit/${promotion.id}`} className="promotion-card__edit-button">
+            Editar
+          </Link>
+        </footer>
+      </div>
     </div>
-);
-
-export default PromotionCard;
+  );
+  
+  export default PromotionCard;
--- src/components/Promotion/List/List.js
+++ src/components/Promotion/List/List.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PromotionCard from '../Card/Card';
-import '../List/List.css'
+import UIModal from '../../UI/Modal/Modal'; 
+import '../List/List.css';
 
 const PromotionList = ({ loading, error, promotions }) => {
+    const [promotionId, setPromotionId] = useState(null);
+
     if(error){
         return <div>Algo de errado não está certo</div>;
     }    
@@ -17,8 +20,14 @@ const PromotionList = ({ loading, error, promotions }) => {
     return (
         <div className="promotion-list">
             {promotions.map((promotion) => (
-                <PromotionCard promotion={promotion} />
+                <PromotionCard 
+                    promotion={promotion} 
+                    onClickComments={() => setPromotionId(promotion.id)}    
+                />
             ))}
+            <UIModal isOpen={Boolean(promotionId)} onClickClose={() => setPromotionId(null)}>
+                <h1>Comentários</h1>
+            </UIModal>
         </div>
     );
 }
